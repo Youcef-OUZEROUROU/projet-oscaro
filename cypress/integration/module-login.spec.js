46eda3dc41b18e76f483596633f6798c79887464
Youcef Ouzerourou <cefyou87@gmail.com>
2020-11-09 10:00:57 +0100

    second commit

--- cypress/integration/module-login.spec.js
+++ cypress/integration/module-login.spec.js
@@ -7,64 +7,64 @@ describe('Téster le login oscaro', () => {
     })
     
     it('testé le champ email vide', () => {
-        cy.get('#login/email').type(' ')
-        cy.get('#login/password').type('Youcef87@')
+        cy.get('#login/email').type(' ').should('have.value',' ')
+        cy.get('#login/password').type('Youcef87@').should('have.value','Youcef87@') 
          cy.get('#login/remember-me').check({force:true}) 
          cy.get('.form-message').should('contain','Le champ "Email" est obligatoire')
     })
     it('testé le champ email que avec des chiffre', () => {
-        cy.get('#login/email').type('654654')
-        cy.get('#login/password').type('Youcef87@')
+        cy.get('#login/email').type('654654').should('have.value','654654')
+        cy.get('#login/password').type('Youcef87@').should('have.value','Youcef87@')
          cy.get('#login/remember-me').check({force:true}) 
          cy.get('.form-message').should('contain','Adresse e-mail incorrecte')
     })
     it('testé le champ email que avec des lettres', () => {
-        cy.get('#login/email').type('klfdlflk,df')
-        cy.get('#login/password').type('Youcef87@')
+        cy.get('#login/email').type('klfdlflkdf').should('have.value','klfdlflkdf')
+        cy.get('#login/password').type('Youcef87@').should('have.value','Youcef87@')
          cy.get('#login/remember-me').check({force:true}) 
          cy.get('.form-message').should('contain','Adresse e-mail incorrecte')
        
     })
     it('testé le champ email sans @', () => {
-        cy.get('#login/email').type('youcefyopmail.com')
-        cy.get('#login/password').type('Youcef87@')
+        cy.get('#login/email').type('youcefyopmail.com').should('have.value','youcefyopmail.com')
+        cy.get('#login/password').type('Youcef87@').should('have.value','Youcef87@')
          cy.get('.form-message').should('contain','Adresse e-mail incorrecte')
        
     })
     it("testé le champ email sans la fin de l'email", () => {
-        cy.get('#login/email').type('youcef@yopmail')
-        cy.get('#login/password').type('Youcef87@')
+        cy.get('#login/email').type('youcef@yopmail').should('have.value','youcef@yopmail')
+        cy.get('#login/password').type('Youcef87@').should('have.value','Youcef87@')
          cy.get('.form-message').should('contain','Adresse e-mail incorrecte')
        
     })
     it('testé le champ password vide', () => {
-        cy.get('#login/email').type('youcef@yopmail.com')
-        cy.get('#login/password').type(' ')
+        cy.get('#login/email').type('youcef@yopmail.com').should('have.value','youcef@yopmail.com')
+        cy.get('#login/password').type(' ').should('have.value',' ')
          cy.get('.form-message').should('contain','Champ obligatoire')
        
     })
     it('testé le champ password que avec des chiffres', () => {
-        cy.get('#login/email').type('youcef@yopmail.com')
-        cy.get('#login/password').type(' ')
-         cy.get('.form-message').should('contain','Champ obligatoire')   
+        cy.get('#login/email').type('youcef@yopmail.com').should('have.value','youcef@yopmail.com')
+        cy.get('#login/password').type('4654646').should('have.value','4654646')
+         cy.get('.form-message').should('contain',"L'adresse e-mail ou le mot de passe ne correspondent pas.")   
        
     })
     it('testé le champ password que avec des lettres et faux', () => {
-        cy.get('#login/email').type('youcef@yopmail.com')
-        cy.get('#login/password').type('lkdfkl')
+        cy.get('#login/email').type('youcef@yopmail.com').should('have.value','youcef@yopmail.com')
+        cy.get('#login/password').type('lkdfkl').should('have.value','lkdfkl')
         cy.get('.toast').should('contain',"L'adresse e-mail ou le mot de passe ne correspondent pas.")
        
     })
     
     it('testé le champ email faux avec un password correct', () => {
-        cy.get('#login/email').type('youcef55@yopmail.com')
-        cy.get('#login/password').type('Youcef87@')
+        cy.get('#login/email').type('youcef55@yopmail.com').should('have.value','youcef55@yopmail.com')
+        cy.get('#login/password').type('Youcef87@').should('have.value','Youcef87@')
         cy.get('.toast').should('contain',"L'adresse e-mail ou le mot de passe ne correspondent pas.")
     })
 
     it('testé le champ email correct avec un password correct', () => {
-        cy.get('#login/email').type(' ')
-        cy.get('#login/password').type('Youcef87@')
+        cy.get('#login/email').type('youcef@yopmail.com').should('have.value','youcef@yopmail.com')
+        cy.get('#login/password').type('Youcef87@').should('have.value','Youcef87@')
          cy.get('#login/remember-me').check({force:true}) 
          cy.url().should('contain','/account/home')
        
